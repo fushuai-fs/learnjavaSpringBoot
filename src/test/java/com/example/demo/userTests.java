bb39b09985c25d1ae9b05149d096bfc7ec123d60
=fus <fushuai_f@126.com>
2018-03-06 17:42:04 +0800

    javalearn druid

--- src/test/java/com/example/demo/userTests.java
+++ src/test/java/com/example/demo/userTests.java
@@ -1,4 +1,35 @@
 package com.example.demo;
 
+import com.example.demo.domain.User;
+import com.example.demo.mapper.UserMapper;
+import org.junit.Test;
+import com.example.demo.param.userParam;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class userTests {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public  void test(){
+
+        userParam userParam = new userParam();
+        userParam.setUserName("aa");
+        userParam.setCurrentPage(0);
+        List<User> users = userMapper.getList(userParam);
+        long count = userMapper.getCount(userParam);
+
+
+        System.out.println(users);
+        System.out.println("总数="+count);
+    }
 }
+
