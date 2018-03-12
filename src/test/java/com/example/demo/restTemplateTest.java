db8f6d81fcd8a515841e312dccdafd07202ca671
=fus <fushuai_f@126.com>
2018-03-12 17:21:27 +0800

    javalearn RestTemplate

--- src/test/java/com/example/demo/restTemplateTest.java
+++ src/test/java/com/example/demo/restTemplateTest.java
@@ -11,13 +11,32 @@ import org.springframework.web.client.RestTemplate;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class restTemplateTest {
-//    @Autowired
+   @Autowired
     private RestTemplate restTemplate;
 
+
+    @Test
+    public void testconfig(){
+        Object object = this.restTemplate.postForObject("http://172.16.2.162:801/api/App/hotel","",Object.class);
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println(object);
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+    }
+
+
+/**
+ * RestTemplate restTemplate = new RestTemplate();
+ * */
     @Test
     public void test(){
-        this.restTemplate = new RestTemplate();
-        Object object = this.restTemplate.postForObject("http://172.16.2.162:801/api/App/hotel","",Object.class);
+        RestTemplate restTemplate = new RestTemplate();
+        Object object = restTemplate.postForObject("http://172.16.2.162:801/api/App/hotel","",Object.class);
 
         System.out.println(object);
     }
