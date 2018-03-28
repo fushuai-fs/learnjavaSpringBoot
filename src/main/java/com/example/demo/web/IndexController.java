efd455f02e09ac3aedc8896ba05f28f93dcb4379
=fus <fushuai_f@126.com>
2018-03-28 14:39:08 +0800

    javalearn

--- src/main/java/com/example/demo/web/IndexController.java
+++ src/main/java/com/example/demo/web/IndexController.java
@@ -1,12 +1,25 @@
 package com.example.demo.web;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-@Controller
+@RestController
 public class IndexController {
 
+    @RequestMapping(path = "/siderBar",method = {RequestMethod.GET})
+    public Map<String, Object> siderBar(HttpServletRequest request, int ID) {
+
+        Map<String,Object> map = new HashMap<>();
+        map.put("msg","success");
+        return map;
+    }
 
 }
